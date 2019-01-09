6775eb380c0a024139bda85149570e505c4303e1
Martin <martin.turk@gmail.com>
2019-01-09 16:33:53 +0100

    upload

--- mrc_routines.py
+++ mrc_routines.py
@@ -38,50 +38,6 @@ class mrc:
     54 213–216 Char MACHSTMachine stamp
     55 217–220 Float32 RMSRMS deviation of map from mean density
     """
-    def __init__(self):
-        # self.path2 = path2
-        # self.header = None
-        self.headerext = None
-        self.image = None
-        # print(self.__dict__)
-
-    def header(self, path):
-        self.path = path
-        with open(self.path, "rb") as f:
-            header = 220 # 220 bytes
-            data = f.read(header)
-            self.nx = struct.unpack('@i', data[0:4])
-            self.ny = struct.unpack('@i', data[4:8])
-            self.nz = struct.unpack('@i', data[8:12])
-            self.mode = struct.unpack('@i', data[12:16]) # ...
-            self.mx = struct.unpack('@i', data[28:32])
-            self.my = struct.unpack('@i', data[32:36])
-            self.mz = struct.unpack('@i', data[36:40])
-            self.cella = struct.unpack('@3f', data[40:52])
-            self.cellangles = struct.unpack('@3f', data[52:64]) # ...
-            self.cellzzz = struct.unpack('@3f', data[64:76]) # ...
-            self.dmin = struct.unpack('@f', data[76:80])
-            self.dmax = struct.unpack('@f', data[80:84])
-            self.mean = struct.unpack('@f', data[84:88])
-            self.ispg = struct.unpack('@i', data[88:92])
-            self.nsymbt = struct.unpack('@i', data[92:96])
-            # self.nsymbtzzz = struct.unpack('@2i', data[96:104]) # ...
-            self.char = struct.unpack('@4c', data[104:108])
-            self.nversion = struct.unpack('@i', data[108:112])
-            # self.gapzzz = struct.unpack('@21f', data[112:196]) # ...
-            self.origin = struct.unpack('@3f', data[196:208])
-            self.map = struct.unpack('@4c', data[208:212])
-            self.machst = struct.unpack('@4c', data[212:216])
-            self.rms = struct.unpack('@f', data[216:220])
-
-            self.pxsize = self.cella[0] / self.nx[0]
-            """
-            Word 24 contains the item NSYMBT in the MRC/CCP4 format (see Table 1), which gives the number\ 
-             of bytes used for symmetry data inserted between the main header (1024 bytes in length) and the data block
-            from: https://doi.org/10.1016/j.jsb.2015.04.002
-            """
-
-            self.start = 1024 + self.nsymbt[0]
 
     def read_mrc(self, path):
         """
@@ -89,43 +45,110 @@ class mrc:
         """
         self.path = path
         with open(self.path, "rb") as f:
-            image_data = f.read()
-
+            mrc_file = f.read()
 
         # header
+        self._header(mrc_file)
+        self._pxsize
+        self._data_mode
+        self._check_size
+        
+        image  = np.frombuffer(mrc_file, dtype=self.dtype, count=-1, offset=self.header_size)
+        self.img = np.reshape(image, (self.nx[0], self.ny[0], self.nz[0]))            
+  
+        # return self.img
 
 
+    def _header(self, mrc_file):
+        # convention from http://www.ccpem.ac.uk/mrc_format/mrc2014.php#note8
+        self.nx = struct.unpack('@i', mrc_file[0:4])
+        self.ny = struct.unpack('@i', mrc_file[4:8])
+        self.nz = struct.unpack('@i', mrc_file[8:12])
+        self.mode = struct.unpack('@i', mrc_file[12:16])
+        self.nxstart = struct.unpack('@i', mrc_file[16:20])
+        self.nystart = struct.unpack('@i', mrc_file[20:24])
+        self.nzstart = struct.unpack('@i', mrc_file[24:28])
+        self.mx = struct.unpack('@i', mrc_file[28:32])
+        self.my = struct.unpack('@i', mrc_file[32:36])
+        self.mz = struct.unpack('@i', mrc_file[36:40])
+        self.cella = struct.unpack('@3f', mrc_file[40:52])
+        self.cellb = struct.unpack('@3f', mrc_file[52:64])
+        self.mapc = struct.unpack('@i', mrc_file[64:68])
+        self.mapr = struct.unpack('@i', mrc_file[68:72])
+        self.maps = struct.unpack('@i', mrc_file[72:76])
+        self.dmin = struct.unpack('@f', mrc_file[76:80])
+        self.dmax = struct.unpack('@f', mrc_file[80:84])
+        self.mean = struct.unpack('@f', mrc_file[84:88])
+        self.ispg = struct.unpack('@i', mrc_file[88:92])
+        self.nsymbt = struct.unpack('@i', mrc_file[92:96])
+        # self.extra = struct.unpack('@220s', mrc_file[0:220]) # ...
+        self.exttyp = struct.unpack('@4c', mrc_file[104:108])
+        self.nversion = struct.unpack('@i', mrc_file[108:112]) # ...
+        self.origin = struct.unpack('@3f', mrc_file[196:208])
+        self.map = struct.unpack('@4s', mrc_file[208:212])[0].decode()
+        self.machst = struct.unpack('@i', mrc_file[212:216])[0] # endiannes
+        self.rms = struct.unpack('@f', mrc_file[216:220])
+        self.nlabl = struct.unpack('@i', mrc_file[220:224])
+        # self.labels = struct.unpack('@800s', mrc_file[224:1024])[0].decode()
 
+        """
+        Word 24 contains the item NSYMBT in the MRC/CCP4 format (see Table 1), which gives the number\ 
+         of bytes used for symmetry data inserted between the main header (1024 bytes in length) and the data block
+        from: https://doi.org/10.1016/j.jsb.2015.04.002
+        """
+        self.header_size = 1024 + self.nsymbt[0]
 
-        b  = np.frombuffer(image_data, dtype=np.int32, count=-1, offset=1024)
-        self.img = np.reshape(b, (160,160,160))            
+    @property
+    def _pxsize(self):
+        self.pxsize = self.cella[0] / self.nx[0]
+        return self.pxsize
 
-        return self.img
+    @property
+    def _data_mode(self):
+        """
+        bytes 13-16 = mode:
+        0 8-bit signed integer (range -128 to 127) 
+        1 16-bit signed integer
+        2 32-bit signed real
+        3 transform : complex 16-bit integers
+        4 transform : complex 32-bit reals
+        6 16-bit unsigned integer
+        """
+        if self.mode[0] == 0:
+            self.dtype = np.int8
 
-        # image = Image.open(io.BytesIO(image_data[self.start : self.get_size]))
-        #image.show()
-        # print(a.byteorder)
+        elif self.mode[0] == 1:
+            self.dtype = np.int16
 
-        # a = np.frombuffer(image_data, dtype=int, count=-1, offset=0)
-        # return a
+        elif self.mode[0] == 2:
+            self.dtype = np.float32
 
+        elif self.mode[0] == 3:
+            self.dtype = None # no native dtype, needs np.dtype([('re', np.int16), ('im', np.int16)])
 
+        elif self.mode[0] == 4:
+            self.dtype = np.complex32
 
-    def check_size(self):
+        elif self.mode[0] == 6:
+            self.dtype = np.uint16
+        
+        return self.dtype
+
+    @property
+    def _check_size(self):
         """
         check whether or not the mrc file has size expected from header
         """
-        expected_size = 1024 + self.nsymbt[0] * 80 * 4 + self.nx[0] * self.ny[0] * self.nz[0] * 4 # for test, mode is ignored
-        file_size = self.get_size
+        expected_size = 1024 + self.nsymbt[0] + self.nx[0] * self.ny[0] * self.nz[0] * np.dtype(self.dtype).itemsize
+        file_size = os.path.getsize(self.path)
+        image_size = self.nx[0] * self.ny[0] * self.nz[0] * np.dtype(self.dtype).itemsize
 
-        print('check file size: expected size = {} actual size = {}'.format(expected_size, file_size))
+        print('check file size: \nexpected size = {} actual size = {}'.format(expected_size, file_size))
+        print('header: {} image({},{},{},{} bytes): {}'.format(self.header_size, self.nx[0], self.ny[0], self.nz[0], 
+                                                        np.dtype(self.dtype).itemsize, image_size))
         if expected_size != file_size:
             print("file size not matching header info!!!")
 
-
-    @property
-    def get_size(self):
-        return os.path.getsize(self.path)
 
     @property
     def headerprint(self):
@@ -135,29 +158,19 @@ class mrc:
         #     keys.append([key, value])
         # return self.__dict__
 
-    def addsth(self, add):
-        self.add = add
-        return self.add + 'ssdsdsd'
-
-
-
 
 def main():
     
     path = '/Users/martin/wrk/run1.mrc'
+    # path = '/home/martin/wrk/run87_class001.mrc'
     test = mrc()
-    test.header(path)
-    # test.header('/home/martin/wrk/run87_class001.mrc')
+    test.read_mrc(path)
+    # print(test.headerprint)
 
-    print( test.headerprint )
-    print( test.get_size )
-    test.check_size()
-
-    print( test.addsth('martin'))
-
-    a = test.read_mrc(path)
-    e = 'main() end'
-    print(e)
+    # # image display
+    # plt.pyplot.imshow(test.img[80]) 
+    # plt.pyplot.gray()
+    # plt.pyplot.show()
 
 if __name__ == "__main__":
     main()
